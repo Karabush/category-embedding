5a0310387d815a3b0c887fce395b52708021bbe3
user <eu1234@gmail.com>
2026-02-16 13:13:34 +0100

    Bump to v0.2.1

--- category_embedding/__init__.py
+++ category_embedding/__init__.py
@@ -1,4 +1,9 @@
 from .encoder import CategoryEmbedding
-from .version import __version__
 
-__all__ = ["CategoryEmbedding", "__version__"]
+__all__ = ["CategoryEmbedding"]
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"  # During development before install
